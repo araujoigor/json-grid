c88f67fb1c019100f3841acaea5bb523c661d69d
Igor Araujo <igor.araujo91@gmail.com>
2020-12-20 18:47:49 +0100

    lint JSONGrid.js

--- JSONGrid.js
+++ JSONGrid.js
@@ -20,51 +20,65 @@ var DOMHelper = {
 
     return element;
   },
-  createJsonGridContainerElement: function (data, domType, dataType, additionalClasses, id) {
-    // -- Anything that is not a valid object/array should be stringified 
+  createJsonGridContainerElement: function (
+    data,
+    domType,
+    dataType,
+    additionalClasses,
+    id
+  ) {
+    // -- Anything that is not a valid object/array should be stringified
     // -- (undefined and null included)
-    var value = typeof data === 'object' && data
-      ? new JSONGrid(data).generateDOM()
-      : DOMHelper.createElement('span', typeof data, 'value');
+    var value =
+      typeof data === 'object' && data
+        ? new JSONGrid(data).generateDOM()
+        : DOMHelper.createElement('span', typeof data, 'value');
 
     if (!value.innerHTML) {
       value.textContent = '' + data;
     }
 
-    var container = DOMHelper.createElement(domType, dataType, additionalClasses, id);
+    var container = DOMHelper.createElement(
+      domType,
+      dataType,
+      additionalClasses,
+      id
+    );
     container.appendChild(value);
 
     return container;
   },
   createExpander: function (dataItems, target) {
     var expander = DOMHelper.createElement('span', 'expander');
-    expander.textContent = '[' + DOMHelper.getExpanderSign(target) + '] ' + dataItems + ' items';
+    expander.textContent =
+      '[' + DOMHelper.getExpanderSign(target) + '] ' + dataItems + ' items';
     expander.setAttribute(DOMHelper.EXPANDER_TARGET_ATTRIBUTE, target.id);
     expander.onclick = DOMHelper.onExpanderClick;
     return expander;
   },
   onExpanderClick: function (event) {
-    var tableId = event.target.getAttribute(DOMHelper.EXPANDER_TARGET_ATTRIBUTE);
+    var tableId = event.target.getAttribute(
+      DOMHelper.EXPANDER_TARGET_ATTRIBUTE
+    );
     var target = document.getElementById(tableId);
-
     if (target) {
       target.classList.toggle(DOMHelper.TABLE_SHRINKED_CLASSNAME);
-      event.target.textContent = '[' + DOMHelper.getExpanderSign(target) + event.target.textContent.slice(2);
+      event.target.textContent =
+        '[' +
+        DOMHelper.getExpanderSign(target) +
+        event.target.textContent.slice(2);
     }
   },
   getExpanderSign: function (target) {
     return target.classList.contains(DOMHelper.TABLE_SHRINKED_CLASSNAME)
       ? '+'
-      : '-'
-      ;
-  }
-}
+      : '-';
+  },
+};
 
 function JSONGrid(data, container) {
   this.data = data;
-  this.container = container instanceof HTMLElement
-    ? container
-    : null;
+  this.container = container instanceof HTMLElement ? container : null;
   this.instanceNumber = JSONGrid.instances || 0;
   JSONGrid.instances = (JSONGrid.instances || 0) + 1;
 }
@@ -89,21 +103,42 @@ JSONGrid.prototype.processArray = function () {
   // -- Add value as header (for primitive, non-object, types)
   // -- otherwise, they are not shown or shown as arrays (strings)
   headers.appendChild(DOMHelper.createElement('th'));
-  
+
   // -- Add object keys as headers
   keys.forEach(function (value) {
     headers.appendChild(
-      DOMHelper.createJsonGridContainerElement(value, 'th', typeof value, 'header')
+      DOMHelper.createJsonGridContainerElement(
+        value,
+        'th',
+        typeof value,
+        'header'
+      )
     );
   });
 
   var rows = this.data.map(function (obj, index) {
-    var tr = DOMHelper.createElement('tr')
-    tr.appendChild(DOMHelper.createJsonGridContainerElement(index, 'td', typeof index, 'header'));
-    tr.appendChild(DOMHelper.createJsonGridContainerElement(obj, 'td', typeof obj));
+    var tr = DOMHelper.createElement('tr');
+    tr.appendChild(
+      DOMHelper.createJsonGridContainerElement(
+        index,
+        'td',
+        typeof index,
+        'header'
+      )
+    );
+    tr.appendChild(
+      DOMHelper.createJsonGridContainerElement(obj, 'td', typeof obj)
+    );
 
     keys.forEach(function (key) {
-      tr.appendChild(DOMHelper.createJsonGridContainerElement(obj[key], 'td', typeof obj, 'table-wrapper'));
+      tr.appendChild(
+        DOMHelper.createJsonGridContainerElement(
+          obj[key],
+          'td',
+          typeof obj,
+          'table-wrapper'
+        )
+      );
     });
 
     return tr;
@@ -113,12 +148,12 @@ JSONGrid.prototype.processArray = function () {
     headers: [headers],
     rows: rows,
   };
-}
+};
 
 JSONGrid.prototype.processObject = function () {
   var data = this.data;
   var rows = Object.keys(this.data).map(function (key) {
-    var tr = DOMHelper.createElement('tr')
+    var tr = DOMHelper.createElement('tr');
     tr.appendChild(
       DOMHelper.createJsonGridContainerElement(key, 'td', 'string', 'header')
     );
@@ -132,7 +167,7 @@ JSONGrid.prototype.processObject = function () {
     headers: [],
     rows: rows,
   };
-}
+};
 
 JSONGrid.prototype.generateDOM = function () {
   var dom;
@@ -153,14 +188,19 @@ JSONGrid.prototype.generateDOM = function () {
     DOMHelper.JSON_GRID_ELEMENT_CONTAINER_CLASSNAME
   );
   var tableId = 'table-' + this.instanceNumber;
-  var intialClasses = this.instanceNumber !== 0 ? [DOMHelper.TABLE_SHRINKED_CLASSNAME] : [];
+  var intialClasses =
+    this.instanceNumber !== 0 ? [DOMHelper.TABLE_SHRINKED_CLASSNAME] : [];
   var table = DOMHelper.createElement('table', 'table', intialClasses, tableId);
   var tbody = DOMHelper.createElement('tbody');
   var expander = DOMHelper.createExpander(dom.rows.length, table);
   container.appendChild(expander);
 
-  dom.headers.forEach(function (val) { tbody.appendChild(val); });
-  dom.rows.forEach(function (val) { tbody.appendChild(val); });
+  dom.headers.forEach(function (val) {
+    tbody.appendChild(val);
+  });
+  dom.rows.forEach(function (val) {
+    tbody.appendChild(val);
+  });
 
   table.appendChild(tbody);
 
